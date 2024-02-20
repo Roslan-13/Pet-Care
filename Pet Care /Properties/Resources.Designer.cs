e1b62f926391acc731463573c2170889bcf611c6
Roslan-13 <131228309+Roslan-13@users.noreply.github.com>
2024-02-19 21:12:05 -0500

    Update Resources.Designer.cs

--- Pet Care /Properties/Resources.Designer.cs
+++ Pet Care /Properties/Resources.Designer.cs
@@ -1,1 +1,17 @@
+namespace PetFriends.Properties
+{
+[global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+    [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    internal class Resources
+    {
+
+        private static global::System.Resources.ResourceManager resourceMan;
+
+        private static global::System.Globalization.CultureInfo resourceCulture;
+
+        [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+        internal Resources()
+        {
+        }
 
