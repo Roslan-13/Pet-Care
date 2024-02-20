386477c19bf965613afa0f074379d404ca5d4cea
Roslan-13 <131228309+Roslan-13@users.noreply.github.com>
2024-02-19 21:15:16 -0500

    Update AssemblyInfo.cs

--- Pet Care /Properties/AssemblyInfo.cs
+++ Pet Care /Properties/AssemblyInfo.cs
@@ -1,2 +1,18 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
+[assembly: AssemblyTitle("PetFriends")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PetFriends")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 
