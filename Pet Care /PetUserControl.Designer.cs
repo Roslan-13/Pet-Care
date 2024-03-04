b11d8dda27332dbe9acfdbdd7ccc5f7667c737c9
Roslan-13 <131228309+Roslan-13@users.noreply.github.com>
2024-03-04 10:21:03 -0500

    Create PetUserControl.Designer.cs
    **PetCare.PetUserControl.cs:**
    
    Within the "PetCare" namespace, this C# code defines a `PetUserControl` representing a user interface component for displaying information about a pet. It includes XML comments for documentation, aiding developers in understanding the code. The class incorporates a set of properties and controls such as `PetPic`, `Pet_C`, `Pet_T`, `Pet_D`, `Pet_L`, `Post_P`, and `Post_C`, which are utilized to present various details about a pet, such as category, description, location, and poster information. Additionally, it features an ellipsis effect using Guna UI to enhance the visual appearance. The class provides a structured and visually appealing way to showcase pet-related information in the "PetCare" application.

---
+++ Pet Care /PetUserControl.Designer.cs
@@ -0,0 +1,111 @@
+namespace PetCare
+{
+    partial class PetUserControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.PetPic = new System.Windows.Forms.PictureBox();
+            this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.guna2Elipse2 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.Pet_C = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.Pet_T = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.Pet_D = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.Pet_L = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.Post_P = new System.Windows.Forms.Label();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.Post_C = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.MoreInfoBtn = new System.Windows.Forms.PictureBox();
+            this.PetPic2 = new System.Windows.Forms.PictureBox();
+            this.PetPic3 = new System.Windows.Forms.PictureBox();
+            this.PetPic4 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PetPic)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.panel6.SuspendLayout();
+            this.panel7.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MoreInfoBtn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PetPic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PetPic3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PetPic4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(185)))), ((int)(((byte)(198)))), ((int)(((byte)(203)))));
+            this.panel1.Controls.Add(this.PetPic);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(146, 186);
+            this.panel1.TabIndex = 0;
+            // 
+            // PetPic
+            // 
+            this.PetPic.Location = new System.Drawing.Point(6, 3);
+            this.PetPic.Name = "PetPic";
+            this.PetPic.Size = new System.Drawing.Size(132, 179);
+            this.PetPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PetPic.TabIndex = 0;
+            this.PetPic.TabStop = false;
+            // 
+            // guna2Elipse1
+            // 
+            this.guna2Elipse1.BorderRadius = 18;
+            this.guna2Elipse1.TargetControl = this;
+            // 
+            // guna2Elipse2
+            // 
+            this.guna2Elipse2.BorderRadius = 18;
+            this.guna2Elipse2.TargetControl = this.panel1;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.Pet_C);
+            this.panel2.Location = new System.Drawing.Point(151, 10);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(253, 31);
+            this.panel2.TabIndex = 105;
+            // 
+            // Pet_C
+            // 
+            this.Pet_C.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Pet_C.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
+            this.Pet_C.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(97)))), ((int)(((byte
