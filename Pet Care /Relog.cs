ad500c918209ba697701d43f6882d6bd77950cb1
Nidhiiir <131202460+Nidhiiir@users.noreply.github.com>
2024-03-04 23:17:34 -0500

    Update Relog.cs

--- Pet Care /Relog.cs
+++ Pet Care /Relog.cs
@@ -9,3 +9,11 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
 using System.Configuration;
+namespace PetFriends
+{
+    public partial class RegLog : Form
+    {
+        public RegLog()
+        {
+            InitializeComponent();
+        }
