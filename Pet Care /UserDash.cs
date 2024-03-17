6d503d6172dff9ae6349970fe0e052d22f2d1971
Nidhiiir <131202460+Nidhiiir@users.noreply.github.com>
2024-03-16 23:13:37 -0400

    Update UserDash.cs
    System.Linq introduces LINQ for powerful querying on data collections.
    ystem.Text imports tools for text manipulation like StringBuilder and StringReader/StringWriter.
    Using System.Threading.Tasks allows for effective, flexible task-oriented automatic programming.
    Using System.Windows.Forms Grants access to building GUIs for desktop applications.
    Using System.IO provides functionalities for input/output operations, such as file and stream handling.
    Using System.Data.SqlClient allows connection and interaction with SQL Server databases.
    Using System.Configuration allows accessing and managing configuration settings in applications.
    Using System.Drawing.Drawing2D offers advanced drawing features like transformations and gradients.
    Using System.Reflection enables runtime type discovery and member calling.

--- Pet Care /UserDash.cs
+++ Pet Care /UserDash.cs
@@ -3,4 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Drawing.Drawing2D;
+using System.Reflection;
